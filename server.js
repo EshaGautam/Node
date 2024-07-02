8e4ace89bb6c823c44195917b395c8c0b2e0c247
Esha Gautam <eshagautam0@gmail.com>
2024-07-02 11:49:10 +0530

    created routes in node.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+
+    if(req.url==='/'){
+        res.setHeader('Content-Type','text/html')
+        res.write(`<html>`)
+        res.write(`<head><title>hey</title></head>`)
+        res.write(`<body><h1>Welcome Back!</h1></body>`)
+        res.write(`</html>`)
+       return res.end()
+    }
+
+   else if(req.url==='/home'){
+        res.setHeader('Content-Type','text/html')
+        res.write(`<html>`)
+        res.write(`<head><title>hey</title></head>`)
+        res.write(`<body><h1>Welcome to home page</h1></body>`)
+        res.write(`</html>`)
+       return res.end()
+    }
+
+   else if(req.url==='/about'){
+        res.setHeader('Content-Type','text/html')
+        res.write(`<html>`)
+        res.write(`<head><title>hey</title></head>`)
+        res.write(`<body><h1>Welcome to about js</h1></body>`)
+        res.write(`</html>`)
+       return res.end()
+    }
+
+   else if(req.url==='/node'){
+        res.setHeader('Content-Type','text/html')
+        res.write(`<html>`)
+        res.write(`<head><title>hey</title></head>`)
+        res.write(`<body><h1>Welcome to my node js project</h1></body>`)
+        res.write(`</html>`)
+       return res.end()
+    }
+   
+})
+
+server.listen(4000)
