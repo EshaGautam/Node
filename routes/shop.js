c05535a2b2ca98e04c43031bc0946f64b1cc77b7
Esha Gautam <eshagautam0@gmail.com>
2024-07-04 12:56:00 +0530

    create clean routes and added page not found

---
+++ routes/shop.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+
+
+app.get('/',(req,res,next)=>{
+    res.send('Its a home page')
+})
+
+
+
+
+module.exports = router
+
+
+
--- routing.js
+++ routing.js
@@ -2,22 +2,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 const app = express()
 
 app.use(bodyParser.urlencoded({'extended':false}))
 
-app.use('/add-products',(req,res,next)=>{
-    res.send(`<form action='/products' method='POST'><input type='text' name='product_name'></input><input type='number' name='product_size'></input><button type='submit'>add</button></form>`)
-})
+// incase we have routes which is like this admin/add-products then we will add common routes
 
-app.post('/products',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
-})
-
+app.use('/admin',adminRoutes)
+app.use(shopRoutes)
 
 app.use('/',(req,res,next)=>{
-    res.send('Its a home page')
+    res.status(404).send(`<h1>Page Not Found</h1>`)
 })
+
 
 app.listen(4000)
